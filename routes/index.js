20f2c1d15a89465f734d965d3cc240d737e7ac3a
Daniel St Andrews <103073945+standrewsdaniel@users.noreply.github.com>
2022-11-13 15:22:12 -0500

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://root:<toor>@cluster0.twmyhj0.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+module.exports = router;
